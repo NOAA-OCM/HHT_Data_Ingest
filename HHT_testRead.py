f7b375ecd6464ddd284dabd8c65dfafaed50bb62
David L Eslinger <udave2007@gmail.com>
2014-12-18 13:36:38 -0500

    HURDAT2 import working to build storm object list.  IBTRaCS not yet implimented.

--- HHT_testRead.py
+++ HHT_testRead.py
@@ -7,14 +7,14 @@ Created 2014-12-11 by David L Eslinger (DLE)
     NOAA Office for Coastal Management
     Charleston, SC USA
     
-Revised:
+Revised: 2014-12-18: HURDAT2 import working (DLE)
 
 """
-#import csv
+import csv
 
 """ Declarations and Parameters """
-workDir = "C:/GIS/Hurricane/HHT_Python/" # On Work Machine
-#workDir = "/home/dave/Data/Hurricanes/" # On ZOG
+#workDir = "C:/GIS/Hurricane/HHT_Python/" # On Work Machine
+workDir = "/home/dave/Data/Hurricanes/" # On ZOG
 dataDir = workDir  # Main Data location
 h2AtlRaw = dataDir + "h2ATL.txt"     # HURDAT2 North Atlantic Data
 h2nepacRaw = dataDir + "h2NEPAC.txt" # HURDAT2 NE North Pacific Data
@@ -24,45 +24,50 @@ resultsDir = workDir + "Results/"  #  Location for final data
 
 """ Storm Object """
 class Storm(object):
-    maxw = None
-    minp = None
-    nobs = 0
-    obs = []
-    start_time = ""
-    end_time = ""
-    sstrack = ""
     def __init__(self,uid,name):
         self.uid = uid
         self.name = name
+        self.maxw = -99
+        self.minp = 999
+        self.numsegs = 0
+        self.segs = []
+        self.start_time = None
+        self.end_time = None
+        self.maxsafir = ""
 
 class Observation(object):
-    def __init__(self,time,lat,lon,wsp,pres,nat,ss):
+    def __init__(self,time,lat,lon,wsp,pres,nature):
         self.time = time
-        self.lat = lat
-        self.lon = lon
+        self.startLat = lat
+        self.startLon = lon
         self.wsp = wsp
         self.pres = pres
-        self.nat = nat
-        self.ss = ss
+        self.nature = nature
+
+class Segment(Observation):
+    def __init__(self,time,lat,lon,wsp,pres,nature):
+        self.endLat = None
+        self.endLon = None
+        self.safir = None
 
 """ Create an empty list to hold allStorms
     and initialize the total storm counter """
 allStorms = []
 numStorms = -1
 
-""" Read and QC/QA HURDAT2 data """
+""" Read HURDAT2 data """
 hFiles = [h2AtlRaw, h2nepacRaw]
 #hFiles = [h2AtlRaw]
 for i, file in enumerate(hFiles):
     print (i, file)
-    with open(file, "r") as h2in:
-        """h2reader = csv.reader(h2in, delimiter=",")
+    with open(file, "r") as rawObsFile:
+        """h2reader = csv.reader(rawObsFile, delimiter=",")
         for row in h2reader:
             print(row)"""
         """ Need a manual loop here to read a header record then 
         the rest of the observations """   
         while True: # With this and the below break, read to EOF
-            lineVals = h2in.readline()
+            lineVals = rawObsFile.readline()
             if not lineVals: # Finds EOF
                 break # Break on EOF
 
@@ -72,10 +77,10 @@ for i, file in enumerate(hFiles):
             #print ("vals = ",vals[0],vals[1],vals[2], len(vals))
             thisStorm = Storm(vals[0],vals[1])  # Create new storm using Unique ID and Name
             thisStorm.nobs = vals[2] # Number of Observations
-            print(thisStorm.uid, thisStorm.name, thisStorm.nobs)
+            #print(thisStorm.uid, thisStorm.name, thisStorm.nobs)
 
             for ob in range(int(thisStorm.nobs)):
-                lineVals = h2in.readline()
+                lineVals = rawObsFile.readline()
                 if not lineVals: # Finds EOF
                     break # Break on EOF
                 """ Create a new observation record """
@@ -88,25 +93,57 @@ for i, file in enumerate(hFiles):
                          else -1. * float(vals[4][:4]))
                 lat = (float(vals[5][:5]) if vals[5][5] == "E" 
                         else -1. * float(vals[5][:5]))
-                print(otime, lon, lat)
-                thisObs = Observation(otime,lat,lon,vals[6],
-                                      vals[7],vals[3],None)
-                thisStorm.obs.append(thisObs)
-            """ Should be done with reading new storm data 
-                Now add it to the allStorms data if appropriate """
-            print ("thisStorm name ", thisStorm.name,"has",
-                   len(thisStorm.obs),"observations")            
+                #print(otime, lon, lat)
+                observation = Segment(otime,lat,lon,vals[6],
+                                      vals[7],vals[3])
+                thisStorm.segs.append(observation)
+            
+            """ All observations read for this new storm data 
+                add thisStorm to the allStorms """
+#==============================================================================
+#             print ("thisStorm name ", thisStorm.name,"has",
+#                    thisStorm.nobs, "observations and is index ", numStorms)            
+#==============================================================================
             allStorms.append(thisStorm)
-            print ("Storm", allStorms[numStorms].name,"has ", 
-                   len(allStorms[numStorms].obs))
+#==============================================================================
+#             print ("Storm number ", len(allStorms)," named ",
+#                    allStorms[numStorms].name,"has ", 
+#                    len(allStorms[numStorms].segs), allStorms[numStorms].nobs)
+#==============================================================================
+""" End of HURDAT2 Ingest"""   
 
-""" End of HURDAT2 Ingest and QC/QA """   
+""" Read IBTrACS data 
+    This data is not split by storms, rather every row has all info in it
+    Therefore, we must read the data, find out if it is a new storm, and
+    if it is, then write the previous storm object/information to allStorms 
+    and create a new thisStorm object and populate it with the first 
+    observation.
+    
+    We know the IBTrACS data starts with 3 header rows, then the 4th row
+    is our first legitimate data record.  
+    Initialize the first thisStorm object from that"""
+    
+with open(ibRaw, "r") as rawObsFile:
+    h2reader = csv.reader(rawObsFile, delimiter=",")
+    head1 = h2reader
+    head2 = h2reader
+    head3 = h2reader
+    record = h2reader
+    print(head1, head2, head3, record)
+    for i, row in enumerate(h2reader):
+        print(row)
+        if i > 5:
+            break
+        else:
+            pass
+        
+""" End of IBTrACS Ingest """
 
-""" Read and QC/QA IBTrACS data """
-
-""" End of IBTrACS Ingest and QA/QC """
-
-""" Combine files into one set of storms"""
+""" Combine files into one set of storms
+            Now process it for QA/QC and finding Safir-Simpson value """
+            #thisStorm.nobs = len(thisStorm.segs)
+            
+                
 
 """ For each storm : """
 """     Create segment shapefiles """
